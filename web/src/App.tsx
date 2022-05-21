dfe1e7663a8b41a15b058268bd20b69b682122d9
felipe2640 <felipe_brito09@hotmail.com>
2022-05-21 20:40:25 -0300

    chore(Front end): Change organization off front end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import React, { ReactElement, useState } from 'react'
+import Form from './components/Form'
 import Hero from './components/Hero'
+import Navbar from './components/Nav'
 import { Projects } from './components/Projects'
 
 function App(): ReactElement {
+  const [login, setlogin] = useState(false)
   return (
     <>
+      <Navbar />
       <Hero />
       <Projects />
     </>
