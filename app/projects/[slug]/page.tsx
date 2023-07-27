f5d0d5e97756639b82a4a64ae54afdcd4c840efd
felipe2640 <felipe_brito09@hotmail.com>
2023-07-27 16:44:22 -0300

    Create

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -8,35 +8,35 @@ import { ReportView } from "./view";
 export const revalidate = 60;
 
 type Props = {
-  params: {
-    slug: string;
-  };
+	params: {
+		slug: string;
+	};
 };
 
 export async function generateStaticParams(): Promise<Props["params"][]> {
-  return allProjects
-    .filter((p) => p.published)
-    .map((p) => ({
-      slug: p.slug,
-    }));
+	return allProjects
+		.filter((p) => p.published)
+		.map((p) => ({
+			slug: p.slug,
+		}));
 }
 
 export default async function PostPage({ params }: Props) {
-  const slug = params?.slug;
-  const project = allProjects.find((project) => project.slug === slug);
+	const slug = params?.slug;
+	const project = allProjects.find((project) => project.slug === slug);
 
-  if (!project) {
-    notFound();
-  }
+	if (!project) {
+		notFound();
+	}
 
-  return (
-    <div className="bg-zinc-50 min-h-screen">
-      <Header project={project} />
-      <ReportView slug={project.slug} />
+	return (
+		<div className="bg-zinc-50 min-h-screen">
+			<Header project={project} />
+			<ReportView slug={project.slug} />
 
-      <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
-        <Mdx code={project.body.code} />
-      </article>
-    </div>
-  );
+			<article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
+				<Mdx code={project.body.code} />
+			</article>
+		</div>
+	);
 }
